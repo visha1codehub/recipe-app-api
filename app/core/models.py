09fc579ede9b2fddb4c68b15c3bb702980f177be
Vishal Gupta <itsvishalhack@gmail.com>
2024-03-19 18:02:21 +0530

    Implemented normalize email

--- app/core/models.py
+++ app/core/models.py
@@ -14,7 +14,9 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_field):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_field)
+        if not email:
+            raise ValueError("User must have an email address!")
+        user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         user.save(using=self._db)
 
