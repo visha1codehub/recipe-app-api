091afd6cc87bd588e31f99521902816984f2c7a7
Vishal Gupta <itsvishalhack@gmail.com>
2024-03-20 12:56:37 +0530

    Setup custom django admin.

--- app/core/models.py
+++ app/core/models.py
@@ -38,7 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=254, unique=True)
     name = models.CharField(max_length=254)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
