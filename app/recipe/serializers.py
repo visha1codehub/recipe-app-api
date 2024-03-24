f9346e35a48cc7d6cf10e2bafb9b595150779ba5
Vishal Gupta <itsvishalhack@gmail.com>
2024-03-24 16:26:51 +0530

    Implemented functionalities and tests for Tags create API.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -4,15 +4,43 @@ Serializers for recipe APIs.
 
 from rest_framework import serializers
 
-from core import models
+from core.models import (
+    Recipe,
+    Tag,
+)
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for Tags model."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
+    tags = TagSerializer(many=True, required=False)
+
     class Meta:
-        model = models.Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+
+        return recipe
 
 
 class RecipeDetailSerializer(RecipeSerializer):
@@ -20,12 +48,3 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
-
-
-class TagSerializer(serializers.ModelSerializer):
-    """Serializer for Tags model."""
-
-    class Meta:
-        model = models.Tag
-        fields = ['id', 'name']
-        read_only_fields = ['id']
