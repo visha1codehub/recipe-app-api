f9550a3dcc0a1a16813941dbefe1241c314d36e0
Vishal Gupta <itsvishalhack@gmail.com>
2024-03-24 14:58:00 +0530

    Implemented functionalities and tests for Tags update API.

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -22,6 +22,11 @@ def create_user(email='user@example.com', password='testpass123'):
     return get_user_model().objects.create_user(email, password)
 
 
+def detail_url(tag_id):
+    """Create and return detail url of a tag."""
+    return reverse("recipe:tag-detail", args=[tag_id])
+
+
 class PublicTagsAPITests(TestCase):
     """Test unauthenticated APIs requests."""
 
@@ -66,3 +71,15 @@ class PrivateTagsAPITests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Test updating a tag."""
+        tag = Tag.objects.create(user=self.user, name='Dessert')
+
+        payload = {'name': 'Dinner Food'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
