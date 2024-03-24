a16f520a59922b2eaf0f6119ecac5365ff720255
Vishal Gupta <itsvishalhack@gmail.com>
2024-03-24 13:05:10 +0530

    Implemented Tag model.

--- app/core/models.py
+++ app/core/models.py
@@ -57,6 +57,19 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=254, blank=True)
+    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+    name = models.CharField(max_length=254)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
