862aa6a955fae0f2af9643a99993e2e7edd3a65c
Vishal Gupta <itsvishalhack@gmail.com>
2024-03-26 14:32:08 +0530

    Modifies recipe model for image.

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,7 +19,7 @@ from drf_spectacular.views import (
 )
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
