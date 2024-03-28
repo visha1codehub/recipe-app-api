ca98d74d2e8080dff1f7566ca1478c4870d32575
Vishal Gupta <itsvishalhack@gmail.com>
2024-03-28 23:06:48 +0530

    Added health check Api.

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render  # noqa
+"""
+Core views for app.
+"""
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def health_check(request):
+    """Returns successful response."""
+    return Response({'healthy': True})
